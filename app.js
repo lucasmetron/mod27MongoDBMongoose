ccd0ddbb3c931e0a86f151c54c47d69a6d0fb60d
lucasmetron <lucasmetron@gmail.com>
2021-06-07 14:48:59 -0300

    conectando o mongoose ao banco mongo

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const mongoose = require('mongoose');
+
+
+mongoose.connect('mongodb://localhost/links').then(db => {
+    console.log(db)
+}).catch(error => {
+    console.log(console.error)
+});
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!!')
+})
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})
