66bd3f72b7785a19215d5ab55208e30ec6c8c5c1
lucasmetron <lucasmetron@gmail.com>
2021-06-07 15:07:45 -0300

    conect com listen

--- app.js
+++ app.js
@@ -4,13 +4,12 @@ const port = 3000;
 const mongoose = require('mongoose');
 
 
-mongoose.connect('mongodb://localhost/links').then(db => {
-    console.log(db)
-}).catch(error => {
-    console.log(console.error)
-});
+mongoose.connect('mongodb://localhost/links', { useUnifiedTopology: true, useNewUrlParser: true })
 
+let db = mongoose.connection;
 
+db.on('error', () => { console.log("houve um erro") })
+db.once('open', () => { console.log("banco carregado") })
 
 app.get('/', (req, res) => {
     res.send('Hello World!!')
